072dc14a30dc48d620dbf18a95e7641e5205fde8
Tim Leslie <616382+timleslie@users.noreply.github.com>
2019-03-31 18:32:13 +1100

    Restructure keystone package

--- packages/keystone/index.js
+++ packages/keystone/index.js
@@ -1,1 +1,13 @@
-exports.Keystone = require('./Keystone');
+const Keystone = require('./lib/Keystone');
+const PasswordAuthStrategy = require('./lib/auth/Password');
+const TwitterAuthStrategy = require('./lib/auth/Twitter');
+const { BaseKeystoneAdapter, BaseListAdapter, BaseFieldAdapter } = require('./lib/adapters');
+
+module.exports = {
+  Keystone,
+  PasswordAuthStrategy,
+  TwitterAuthStrategy,
+  BaseKeystoneAdapter,
+  BaseListAdapter,
+  BaseFieldAdapter,
+};
